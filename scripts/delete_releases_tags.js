b5ef1563f6ff4904513748061357e820e5333a86
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-14 10:48:51 +0800

    Update delete_releases_tags.js

--- scripts/delete_releases_tags.js
+++ scripts/delete_releases_tags.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const githubToken = process.env.GITHUB_TOKEN;
+const personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN;
 const repo = process.env.GITHUB_REPOSITORY;
 
 const [owner, repoName] = repo.split('/');
@@ -8,7 +8,7 @@ const [owner, repoName] = repo.split('/');
 const axiosInstance = axios.create({
   baseURL: `https://api.github.com/repos/${owner}/${repoName}`,
   headers: {
-    'Authorization': `token ${githubToken}`,
+    'Authorization': `token ${personalAccessToken}`,
     'Accept': 'application/vnd.github.v3+json',
   }
 });
