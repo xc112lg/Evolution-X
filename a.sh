7feecd5d1027ab3947c66d5dcfdbf16f802ee5a3
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-25 06:41:34 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -4,8 +4,9 @@ rm -rf h870/* h872/* us997/*
 rm Evolution-X/*.zip
 rm Evolution-X/*.img
 rm Evolution-X/*.txt
-crave ssh -- ls
-crave pull out/target/product/*/*.zip out/target/product/*/recovery.img out/target/product/*/*.json out/target/product/*/changelog_*.txt 
+#crave ssh -- ls
+crave pull out/target/product/*/*.zip 
+#out/target/product/*/recovery.img out/target/product/*/*.json out/target/product/*/changelog_*.txt 
 mv h870/recovery.img h870/recoveryh870.img
 mv h872/recovery.img h872/recoveryh872.img
 mv us997/recovery.img us997/recoveryus997.img
